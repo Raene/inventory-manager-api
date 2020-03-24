5d27aa574c461723efd8ea33ea94979207061619
Raene <crofty.52.bu@gmail.com>
2020-03-24 15:07:13 +0100

    added new model for autoloading

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('product_model' => 'product');
+$autoload['model'] = array('product_model' => 'product', 'sales_model' => 'sales');
