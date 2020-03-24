2fc63c8514c0aa4641ba7e070692968ebbd7696f
Raene <crofty.52.bu@gmail.com>
2020-03-24 16:04:32 +0100

    Added methods to fetch sum for month, day, week

--- application/models/Sales_model.php
+++ application/models/Sales_model.php
@@ -58,4 +58,68 @@ class Sales_model extends CI_model
 
         return $query->result_array();
     }
+
+    public function get_sales_by_month()
+    {
+        $sql   = "SELECT * FROM `sales` WHERE MONTH(created_at) = Month(NOW())";
+        $query = $this->db->query($sql);
+
+            if(!$query)
+            {
+                $error = $this->db->error();
+                $errorMessage = $error['message'];
+                $errorStatus = 500;
+                throw new Exception($errorMessage, $errorStatus);
+            }
+
+        return $query->result_array();
+    }
+
+    public function monthly_sum()
+    {
+        $sql = "SELECT SUM(quantity * prod_price) FROM sales WHERE MONTH(created_at) = MONTH(NOW())";
+        $query = $this->db->query($sql);
+
+            if(!$query)
+            {
+                $error = $this->db->error();
+                $errorMessage = $error['message'];
+                $errorStatus = 500;
+                throw new Exception($errorMessage, $errorStatus);
+            }
+
+        return $query->row_array();
+    }
+
+    public function daily_sum()
+    {
+        $sql = "SELECT SUM(quantity * prod_price) FROM sales WHERE DAY(created_at) = DAY(NOW())";
+        $query = $this->db->query($sql);
+
+            if(!$query)
+            {
+                $error = $this->db->error();
+                $errorMessage = $error['message'];
+                $errorStatus = 500;
+                throw new Exception($errorMessage, $errorStatus);
+            }
+
+        return $query->row_array();
+    }
+
+    public function weekly_sum()
+    {
+        $sql = "SELECT SUM(quantity * prod_price) FROM sales WHERE WEEK(created_at) = WEEK(NOW())";
+        $query = $this->db->query($sql);
+
+            if(!$query)
+            {
+                $error = $this->db->error();
+                $errorMessage = $error['message'];
+                $errorStatus = 500;
+                throw new Exception($errorMessage, $errorStatus);
+            }
+
+        return $query->row_array();
+    }
 }
