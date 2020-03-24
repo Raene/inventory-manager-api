c6d7613e7cb65c646ebdddc8529136555f258ea8
Raene <crofty.52.bu@gmail.com>
2020-03-24 15:09:12 +0100

    created sales controller added method to fetch sales by day added method to fetch sales by week added method to fetch all sales

--- application/controllers/Sales.php
+++ application/controllers/Sales.php
@@ -6,6 +6,48 @@ class Sales extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        headersUp();
+    }
+
+    public function index(){
+        try 
+		{
+            $resp["payload"] = $this->sales->get_all();
+            $resp["status"]  = 200;
+
+			return response($resp["status"], $resp["payload"]);
+		}
+        catch (\Throwable $th) 
+		{
+			return response($th->getCode(),$th->getMessage());
+		}
     }
     
+    public function get_sales_today(){
+        try 
+		{
+            $resp["payload"] = $this->sales->get_sales_by_day();
+            $resp["status"]  = 200;
+
+			return response($resp["status"], $resp["payload"]);
+		}
+		catch (\Throwable $th) 
+		{
+			return response($th->getCode(),$th->getMessage());
+		}
+    }
+
+    public function get_sales_week(){
+        try 
+		{
+            $resp["payload"] = $this->sales->get_sales_by_week();
+            $resp["status"]  = 200;
+
+			return response($resp["status"], $resp["payload"]);
+		}
+        catch (\Throwable $th) 
+		{
+			return response($th->getCode(),$th->getMessage());
+		}
+    }
 }
