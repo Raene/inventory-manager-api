a0c5df37beee9c2816e38e04b52065e20c6bdcbe
Raene <crofty.52.bu@gmail.com>
2020-03-26 10:10:31 +0100

    added new helper file for autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','my_response_helper','my_header_setup');
+$autoload['helper'] = array('url','my_response_helper','my_header_setup', 'my_utility');
 
 /*
 | -------------------------------------------------------------------
