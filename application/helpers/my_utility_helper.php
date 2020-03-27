5688573c3901a2f1c8f6b35bbdde941f8643505e
Raene <crofty.52.bu@gmail.com>
2020-03-27 13:23:38 +0100

    added new helper functin

--- application/helpers/my_utility_helper.php
+++ application/helpers/my_utility_helper.php
@@ -7,3 +7,14 @@ function isNot_zero($num){
             return true;
         }       
     }
+
+function email_exists($tablename,$email){
+        $ci = & get_instance();
+        $query = $ci->db->get_where($tablename, array('email' => $email));
+
+        if(empty($query->row_array())){
+            return true;
+        } else{
+            return false;
+        }
+    }
