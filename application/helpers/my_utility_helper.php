7900334045633155d0205a276658b48c0752fc40
Raene <crofty.52.bu@gmail.com>
2020-03-27 14:40:00 +0100

    new helper fetch user by email

--- application/helpers/my_utility_helper.php
+++ application/helpers/my_utility_helper.php
@@ -18,3 +18,22 @@ function email_exists($tablename,$email){
             return false;
         }
     }
+
+function fetch_by_email($tablename,$email)
+{
+        $ci = & get_instance();
+        $query = $ci->db->get_where($tablename, array('email' => $email));
+
+        if(!$query){
+            $error = $ci->db->error();
+            $errorMessage = $error['message'];
+            $errorStatus = 400;
+            throw new Exception($errorMessage, $errorStatus);
+        }
+
+        if($query->num_rows() == 1){
+            return $query->row_array();
+        }else{
+            return false;
+        }
+    }
