119919a79fbef1085fb4b738079f82856d51846e
Raene <crofty.52.bu@gmail.com>
2020-04-03 11:28:01 +0100

    added user_model

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('product_model' => 'product', 'sales_model' => 'sales', 'expense_model'=> 'expense', 'auth_model'=>'auth');
+$autoload['model'] = array('product_model' => 'product', 'sales_model' => 'sales', 'expense_model'=> 'expense', 'auth_model'=>'auth', 'user_model'=>'user');
