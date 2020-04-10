a900434e6a0b7475008b0d854491abfa63fb65e8
Raene <bob.salau@gmail.com>
2020-04-10 07:22:25 -0700

    refactored create function

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -3,40 +3,41 @@
         private $name;
         private $email;
         private $password;
+        private $isadmin;
 
         public function __construct(){
             $this->load->database();
         }
 
-        public function create($tablename,$data)
+        public function create($data)
         {
-            $query = $this->db->insert($tablename, $data);
-
-            if(!$query){
-                $error = $this->db->error();
-                $errorMessage = $error['message'];
-                $errorStatus = 400;
-                throw new Exception($errorMessage, $errorStatus);
-            }
-            return "User Created";
+                $sql = "INSERT INTO `admins`() VALUES ()";
+                $query = $this->db->query($sql);
+                $data['admin_id'] = $this->db->insert_id();
+                $query = $this->db->insert('user',$data);
+                db_error_response($query);
+                return "Admin User Created";
         }
 
-        public function login($tablename,$password,$email)
+        public function login($data)
         {
-            $result = fetch_by_email($tablename,$email);
+            $this->email    = $data['email'];
+            $this->password = $data['password'];
+
+            $result = fetch_by_email('user',$this->email);
             if($result == FALSE){
                 $errorMessage = 'User Not Found';
                 $errorStatus  = 400;
                 throw new Exception($errorMessage, $errorStatus);
             }
 
-             if(!password_verify($password, $result["password"]))
+             if(!password_verify($this->password, $result["password"]))
              {
                  $errorMessage = 'Invalid Password';
                  $errorStatus = 400;
                  throw new Exception($errorMessage, $errorStatus);
              }
 
-             return array('id'=>$result["id"], 'name'=>$result['name'], 'email'=>$result['email']);
+             return array('id'=>$result["id"], 'name'=>$result['name'], 'email'=>$result['email'], 'role'=>$result['role']);
         }
     }
