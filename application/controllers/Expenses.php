d188bb2d15cd8d429c8f935ba400ac7eb430ffc9
Abdulrahman Salau <bob.salau@gmail.com>
2020-04-15 14:40:01 +0100

    added access control for expenses code refactor on expenses

--- application/controllers/Expenses.php
+++ application/controllers/Expenses.php
@@ -3,10 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Expenses extends CI_Controller
 { 
+	private $key;
+	public $creds;
+	public $authHeader;
+ 
     public function __construct()
     {
         parent::__construct();
-        headersUp();
+		headersUp();
+		$this->load->library('myauthorization');
+		$this->authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+		$this->key   =  $this->config->item('jwt-key');
+		
         
     }
 
@@ -14,7 +22,13 @@ class Expenses extends CI_Controller
     {
         try 
 		{
-            $resp["payload"] = $this->expense->get_all();
+			$this->creds = $this->myauthorization->isLoggedIn($this->authHeader, $this->key);
+			if (!$this->myauthorization->isAdmin($this->creds['role'])) 
+			{
+				return response(401, "Only admins may see all expenses");
+			}
+
+            $resp["payload"] = $this->expense->get_all($this->creds['admin_id']);
             $resp["status"]  = 200;
 
 			return response($resp["status"], $resp["payload"]);
@@ -29,11 +43,13 @@ class Expenses extends CI_Controller
 	{
 		try 
 		{
+			$this->creds = $this->myauthorization->isLoggedIn($this->authHeader, $this->key);
 			$data = $this->input->raw_input_stream;
 			$data = utf8_encode($data);
             $data = json_decode($data, true);
             
-            //flatten the array, because CI form validation library has issues reading multidimensional array
+			//flatten the array, because CI form validation library has issues reading multidimensional array
+			//with array flattened we can apply form_validation to all properties
             $out   = [];
             $iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($data));
             foreach($iterator as $key => $val)
@@ -41,7 +57,9 @@ class Expenses extends CI_Controller
                 $out[$key] = $val;
             }
             $this->form_validation->set_data($out);
-            $this->form_validation->set_rules('product_name', 'Product Name', 'required');
+			$this->form_validation->set_rules('product_name', 'Product Name', 'required');
+			$this->form_validation->set_rules('user_id', 'User id', 'required');
+			$this->form_validation->set_rules('admin_id', 'Admin id', 'required');
             $this->form_validation->set_rules('quantity', 'Quantity', 'required|callback_quantity_isNot_zero');
             $this->form_validation->set_rules('product_price', 'Product Price', 'required|callback_price_isNot_zero');
 
@@ -67,7 +85,8 @@ class Expenses extends CI_Controller
     public function get_expenses_today(){
         try 
 		{
-            $resp["payload"] = $this->expense->get_expenses_by_day();
+			$this->creds = $this->myauthorization->isLoggedIn($this->authHeader, $this->key);
+            $resp["payload"] = $this->expense->get_expenses_by_day($this->creds['admin_id']);
             $resp["status"]  = 200;
 
 			return response($resp["status"], $resp["payload"]);
@@ -81,7 +100,8 @@ class Expenses extends CI_Controller
     public function get_expenses_week(){
         try 
 		{
-            $resp["payload"] = $this->expense->get_expenses_by_week();
+			$this->creds = $this->myauthorization->isLoggedIn($this->authHeader, $this->key);
+            $resp["payload"] = $this->expense->get_expenses_by_week($this->creds['admin_id']);
             $resp["status"]  = 200;
 
 			return response($resp["status"], $resp["payload"]);
@@ -95,7 +115,8 @@ class Expenses extends CI_Controller
     public function get_expenses_month(){
         try 
 		{
-            $resp["payload"] = $this->expense->get_expenses_by_month();
+			$this->creds = $this->myauthorization->isLoggedIn($this->authHeader, $this->key);
+            $resp["payload"] = $this->expense->get_expenses_by_month($this->creds['admin_id']);
             $resp["status"]  = 200;
 			return response($resp["status"], $resp["payload"]);
 		}
@@ -106,6 +127,7 @@ class Expenses extends CI_Controller
     }
 
     public function delete($id){
+		$this->creds = $this->myauthorization->isLoggedIn($this->authHeader, $this->key);
 		$resp = $this->expense->delete($id);
 		return response($resp["status"], $resp["message"]); 
 	}
